fb4bccb59db4f00206c2147426c659a8173d3944
adicse20 <adi.cse20@gmail.com>
2022-12-21 06:45:26 +0100

    commit latest code

--- Challenge2/SpringBoootWebApp/webapp/src/main/java/com/azure/webapp/AzureCompute.java
+++ Challenge2/SpringBoootWebApp/webapp/src/main/java/com/azure/webapp/AzureCompute.java
@@ -3,6 +3,8 @@ package com.azure.webapp;
 import com.microsoft.azure.AzureEnvironment;
 import com.microsoft.azure.credentials.ApplicationTokenCredentials;
 import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.compute.DiskInstanceView;
+import com.microsoft.azure.management.compute.InstanceViewStatus;
 import com.microsoft.azure.management.compute.VirtualMachine;
 import com.microsoft.azure.management.resources.fluentcore.arm.Region;
 import com.microsoft.azure.management.storage.StorageAccount;
@@ -23,10 +25,59 @@ public class AzureCompute {
 			    .withSubscription("d57b17c6-c6ae-4051-919f-6f86ca9b0684");
 
 		
-			VirtualMachine vm = azure.virtualMachines().getById("/subscriptions/d57b17c6-c6ae-4051-919f-6f86ca9b0684/resourceGroups/virtual-machine-rg/providers/Microsoft.Compute/virtualMachines/vmcodechallenge2");
+			VirtualMachine vm = azure.virtualMachines().getById("/subscriptions/d57b17c6-c6ae-4051-919f-6f86ca9b0684/resourceGroups/azure-tier/providers/Microsoft.Compute/virtualMachines/web-vm");
 			ObjectMapper mapper = new ObjectMapper();
+				
 			try {
 				System.out.println("value is :::"+mapper.writeValueAsString(vm));
+				System.out.println("hardwareProfile");
+				System.out.println("    vmSize: " + vm.size());
+				System.out.println("storageProfile");
+				System.out.println("  imageReference");
+				System.out.println("    publisher: " + vm.storageProfile().imageReference().publisher());
+				System.out.println("    offer: " + vm.storageProfile().imageReference().offer());
+				System.out.println("    sku: " + vm.storageProfile().imageReference().sku());
+				System.out.println("    version: " + vm.storageProfile().imageReference().version());
+				System.out.println("  osDisk");
+				System.out.println("    osType: " + vm.storageProfile().osDisk().osType());
+				System.out.println("    name: " + vm.storageProfile().osDisk().name());
+				System.out.println("    createOption: " + vm.storageProfile().osDisk().createOption());
+				System.out.println("    caching: " + vm.storageProfile().osDisk().caching());
+				System.out.println("osProfile");
+				System.out.println("    computerName: " + vm.osProfile().computerName());
+				System.out.println("    adminUserName: " + vm.osProfile().adminUsername());
+				System.out.println("networkProfile");
+				System.out.println("    networkInterface: " + vm.primaryNetworkInterfaceId());
+				System.out.println("vmAgent");
+				System.out.println("  vmAgentVersion: " + vm.instanceView().vmAgent().vmAgentVersion());
+				System.out.println("    statuses");
+				for (InstanceViewStatus status : vm.instanceView().vmAgent().statuses()) {
+				    System.out.println("    code: " + status.code());
+				    System.out.println("    displayStatus: " + status.displayStatus());
+				    System.out.println("    message: " + status.message());
+				    System.out.println("    time: " + status.time());
+				}
+				System.out.println("disks");
+				for (DiskInstanceView disk : vm.instanceView().disks()) {
+				    System.out.println("  name: " + disk.name());
+				    System.out.println("  statuses");
+				    for (InstanceViewStatus status : disk.statuses()) {
+				        System.out.println("    code: " + status.code());
+				        System.out.println("    displayStatus: " + status.displayStatus());
+				        System.out.println("    time: " + status.time());
+				    }
+				}
+				System.out.println("VM general status");
+				System.out.println("  provisioningStatus: " + vm.provisioningState());
+				System.out.println("  id: " + vm.id());
+				System.out.println("  name: " + vm.name());
+				System.out.println("  type: " + vm.type());
+				System.out.println("VM instance status");
+				for (InstanceViewStatus status : vm.instanceView().statuses()) {
+				    System.out.println("  code: " + status.code());
+				    System.out.println("  displayStatus: " + status.displayStatus());
+				}
+				
 			} catch (JsonProcessingException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
--- Challenge3/src/main/java/com/function/codechallenge/Function.java
+++ Challenge3/src/main/java/com/function/codechallenge/Function.java
@@ -6,7 +6,6 @@ import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
-import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
@@ -56,18 +55,17 @@ public class Function {
                 methods = {HttpMethod.GET, HttpMethod.POST},
                 authLevel = AuthorizationLevel.ANONYMOUS)
                 HttpRequestMessage<Optional<String>> request,
+                
                // @BindingName("queryValue") String queryValue,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Parse query parameter
         final String query = request.getQueryParameters().get("name");
-        final String query2 = request.getQueryParameters().get("surname");
         
         final String name = request.getBody().orElse(query);
-        final String surname = request.getBody().orElse(query2);
 
-
+        context.getLogger().info("value of input keys is::sssssss");
         final String query3 = request.getQueryParameters().get("fourthCharacter");
         final String fourthCharacter = request.getBody().orElse(query3);
 
@@ -81,10 +79,9 @@ public class Function {
         final String query6 = request.getQueryParameters().get("firstCharacter");
         final String firstCharacter = request.getBody().orElse(query6);
 
-        final String query7 = request.getQueryParameters().get("key");
-        final String key = request.getBody().orElse(query7);
-
-
+        final String query7 = request.getQueryParameters().get("inputkeys");
+       final String inputkeys = request.getBody().orElse(query7);
+    
         Map<String, Object> u = new HashMap<>();
 
 		Map<String, Object> a = new HashMap<>();
@@ -95,22 +92,24 @@ public class Function {
 
         
 
-       /*  String[] keys = key.split("/");
+        String[] keys = inputkeys.split("/");
         Object value = u;
         for (String k : keys) {
             value = ((Map<String, Object>) value).get(k);
-        }	*/
+        }	
 
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            //return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name+":"+surname+":"+fourthCharacter+thirdCharacter+secondCharacter+firstCharacter+key+"|||"+value).build();
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            //return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name+"|"+fourthCharacter+thirdCharacter+secondCharacter+firstCharacter+inputkeys+"|||"+value).build();
+           return request.createResponseBuilder(HttpStatus.OK).body("Value return is:::" + value).build();
         }
     }
 
 
     
+
+
 
 
 }
